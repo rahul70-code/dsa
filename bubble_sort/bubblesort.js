0a2de83d6093b781a2f171c23fbea4c8fad6e498
Rahul Talreja <rahul@orbo.ai>
2020-03-29 00:50:47 +0530

    some changes

---
+++ bubble_sort/bubblesort.js
@@ -0,0 +1,36 @@
+function bubbleSort(array) {
+  if (array.length == 0 || array.length == 1) return array;
+  for (var i = array.length; i>0; i--) {
+    for (var j = 0; j < i-1; j++) {
+        console.log(array,array[j],array[j+1])
+      if (array[j] > array[j+1]) {
+        // swap
+        var temp = array[j];
+        array[j] = array[j+1];
+        array[j+1] = temp;
+      }
+    }
+  }
+  return array;
+};
+
+function bubbleSortOptimization(array) {
+    if (array.length == 0 || array.length == 1) return array;
+    for (var i = array.length; i>0; i--) {
+        var noSwaps = true;
+      for (var j = 0; j < i-1; j++) {
+          console.log(array,array[j],array[j+1])
+        if (array[j] > array[j+1]) {
+          // swap
+          var temp = array[j];
+          array[j] = array[j+1];
+          array[j+1] = temp;
+          noSwaps = false;
+        }
+      }
+      if(noSwaps) break;
+    }
+    return array;
+  }
+
+console.log(bubbleSortOptimization([12, 4, 44, 1, 23,45,55,77]));
