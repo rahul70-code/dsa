d196c7c69e0d3e276ebef8f64a5911660593313c
Rahul Talreja <rahul@orbo.ai>
2020-04-04 12:15:10 +0530

    added graph transversal

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -1,13 +1,46 @@
 class Graph {
-    constructor() {
-        this.adjacencyList = {};
-    };
+  constructor() {
+    this.adjacencyList = {};
+  }
 
-    addVertex(vertex) {
-        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-    };
-};
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  };
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  };
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      v => v !== vertex2
+    );
+
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      v => v !== vertex1
+    );
+  };
+
+  removeVertex(vertex) {
+    while(this.adjacencyList[vertex].length) {
+        const adjacentVertex = this.adjacencyList[vertex].pop();
+        this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+}
 
 var g = new Graph();
-g.addVertex("Tokyo");
-console.log(g)
+g.addVertex("rahul");
+g.addVertex("dilip");
+console.log(g.adjacencyList);
+g.addEdge("rahul", "dilip");
+console.log(g.adjacencyList);
+g.removeEdge("rahul", "dilip");
+console.log(g.adjacencyList);
+g.removeVertex('rahul');
+console.log(g.adjacencyList);
+
+// console.log(g.adjacencyList.push('dilip'))
+// console.log(g.adjacencyList)
