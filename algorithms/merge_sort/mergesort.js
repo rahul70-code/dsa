433057280b9c935e71567a7145bcdf23ad397a9b
Rahul Talreja <rahul@orbo.ai>
2020-04-05 12:46:37 +0530

    added hash tables

---
+++ algorithms/merge_sort/mergesort.js
@@ -0,0 +1,30 @@
+function merge(arr1,arr2) {
+    let results = [];
+    var i = 0;
+    var j = 0;
+    while(i<arr1.length && j< arr2.length) {
+        if(arr2[j]>arr1[i]) {
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j]);
+            j++;
+        };
+    };
+
+    while(i < arr1.length) {
+        results.push(arr1[i]);
+    };
+
+    while(j < arr2.length) {
+        results.push(arr2[j]);
+    }
+
+    return results;
+};
+
+function mergeSort(arr) {
+
+};
+
+console.log(merge([12,32,1],[32,44,2]))
